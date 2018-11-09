012d3c2ae8fbd1bf61e41d87244791afe7ab0caf
陳秉毅 <bing_note@abc>
2018-11-09 23:53:22 +0800

    秉毅 1109 DEMO後 push

--- FinalProject/src/main/java/controller/wish/WishController.java
+++ FinalProject/src/main/java/controller/wish/WishController.java
@@ -1,7 +1,5 @@
 package controller.wish;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +21,7 @@ public class WishController {
 	@Autowired
 	WishService wishService;
 	
+/*========================================================處理會員點選愛心 還是 貨到付款按鈕========================================================*/
 	@RequestMapping(path = "processMemberWish", produces = "text/html;charset=utf-8")
 	@ResponseBody
 	public String processWish(String email, Integer proId,Integer tracked) {
@@ -75,41 +74,10 @@ public class WishController {
 				return "";
 			}
 		}
-//		if("前端傳回的愛心狀態是紅色") {
-//			if(aWishRow!=null) {//有願望清單 更改狀態
-//				aWishRow.setTracked(1);
-//				return "";//尚未確認需要回傳什麼
-//			}else {//沒有願望清單 新增一筆
-//				newWishBean.setMemberid(memberInfo.getMemberid());
-//				newWishBean.setProid(proId);
-//				newWishBean.setTracked(1);
-//				return "";//尚未確認需要回傳什麼
-//			}
-//		}
-//		if("前端傳回的愛心狀態是白色") {
-//			aWishRow.setTracked(0);
-//			return "";//尚未確認需要回傳什麼
-//		}
-//		return "";
-//		if(tracked==2) {
-//			System.out.println("使用者是點選貨到通知的按鈕");
-//			if(aWishRow!=null) {//有願望清單 更改狀態
-//				System.out.println("點選的商品已在願望清單 修改狀態");
-//				aWishRow.setTracked(2);
-//				wishService.update(aWishRow);
-//				return "";
-//			}else {//沒有願望清單 新增一筆
-//				System.out.println("點選的商品不在願望清單 新增一筆");
-//				newWishBean.setMemberid(memberInfo.getMemberid());
-//				newWishBean.setProid(proId);
-//				newWishBean.setTracked(2);
-//				wishService.insertAWishBean(newWishBean);
-//				return "";
-//			}
-//		}
+
 		return "";
 	}
-	
+/*========================================================處理會員近來網站內 商品愛心的處理========================================================*/
 	@RequestMapping(path = "processFirstMemberLoadWish", produces = "text/html;charset=utf-8")
 	@ResponseBody
 	public String processFirstMemberLoadWish(String email, Integer proId) {
@@ -118,8 +86,6 @@ public class WishController {
 		System.out.println("有沒有接收到user點選的商品id??=> " + proId);
 		MemberBean memberBean = loginService.checkEmail(email);
 		System.out.println("透過email有沒有撈取到會員資料" +memberBean);
-		
-	
 			WishBean wishBean;
 			try {//會員有願望清單資料
 				wishBean = wishService.selectByMemberIdProId(memberBean.getMemberid(), proId);
@@ -128,7 +94,24 @@ public class WishController {
 			} catch (Exception e) {//會員沒有願望清單資料
 				return "nowish";
 			}
-	
 	}
+
+
+	@RequestMapping(path = "processMemberSelectWish", produces = "text/html;charset=utf-8")
+	@ResponseBody
+	public String processMemberSelectWish(String email) {
 	
+		loginService.checkEmail(email);
+		
+		
+		
+		
+		return "";
+	}
+
+
+
+
+
+
 }
