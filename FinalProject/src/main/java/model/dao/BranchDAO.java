d10c66625ddc188be9c2c5378048e9770910346c
陳敬諺 <EEIT10347@III.com>
2018-11-19 09:30:27 +0800

    陳敬諺1119
    修改BUG

--- FinalProject/src/main/java/model/dao/BranchDAO.java
+++ FinalProject/src/main/java/model/dao/BranchDAO.java
@@ -5,8 +5,11 @@ import java.util.List;
 import model.bean.BranchBean;
 
 public interface BranchDAO {
-	 List<BranchBean> selectAll();
-	    BranchBean selectById(int id);
-	    BranchBean insert(BranchBean bean);
-	    boolean update(BranchBean bean);
+	List<BranchBean> selectAll();
+
+	BranchBean selectById(int id);
+
+	BranchBean insert(BranchBean bean);
+
+	boolean update(BranchBean bean);
 }
